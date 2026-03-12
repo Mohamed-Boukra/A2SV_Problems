1498e5051d4799025f46533ac7d3689577b004e3
Mohamed Boukra <96997187+Mohamed-Boukra@users.noreply.github.com>
2026-03-12 22:50:14 +0100

    Add solution for B - Ehab Is an Odd Person (trial 1)

---
+++ codeforces/b-ehab-is-an-odd-person_trial1.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+has_odd  = any(x % 2 == 1 for x in a)
+has_even = any(x % 2 == 0 for x in a)
+
+if has_odd and has_even:
+    a.sort()
+
+print(*a)
